da1a4e6b9f68bcbe535b6a2be45247a4ced83770
Bogdanka Dimitrijevic <bogdankadimitrijevic@gmail.com>
2022-07-15 20:56:08 +0200

    Added avatar, messagetext, message, messageform, messagemetadata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { JoinPage } from "./pages/JoinPage";
+import { Message } from "./components/Message";
 
 function App() {
-  return <JoinPage />
+  return (
+    <Message
+      avatarBackgroundColor="green"
+      avatarText="BD"
+      displayName="Bogdanka Dimitrijević"
+      time="20:50"
+    >
+      Ready for the trip? I'm so excited I couldn't sleep!
+    </Message>
+  );
 }
 
 export default App;
